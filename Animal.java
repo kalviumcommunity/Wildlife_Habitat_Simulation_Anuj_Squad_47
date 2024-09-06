9406d5a31dc56da9c779058ea5fb53a2a4d23f33
offialanuj01 <offialanuj004@gmail.com>
2024-09-06 16:33:34 +0530

    milestone_1 this pointer has completed

--- Animal.java
+++ Animal.java
@@ -1,26 +1,25 @@
-
-
 package Wildlife_Habitat_Simulation_Anuj_Squad_47;
+
 public class Animal {
-    // Attributes (Encapsulated properties)
     private String name;
     private String type;
 
-    // Constructor
+    
     public Animal(String name, String type) {
-        this.name = name;
-        this.type = type;
+        this.name = name;  
+        this.type = type;  
     }
 
-    // Member function 1: Get animal details
+
+    public Animal changeType(String newType) {
+        this.type = newType; 
+        return this;          
+    }
+
     public void displayAnimalInfo() {
-        System.out.println("Animal Name: " + name);
-        System.out.println("Animal Type: " + type);
-    }
-
-    // Member function 2: Simulate animal action
-    public void makeSound() {
-        System.out.println(name + " is making a sound!");
+        System.out.println("Animal Name: " + this.name); 
+        System.out.println("Animal Type: " + this.type);
     }
 }
+
 
